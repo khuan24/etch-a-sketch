99904f98ed86f2097e47a3e5522a9ab5bb57e9d2
Kexin Huang <khuan24@uwo.ca>
2025-01-07 13:43:18 -0500

    Change layout to rows and squares

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const gridContainer = document.querySelector(".grid-container")
 const dimension = 16
 
 function makeGrid() {
-    // create a 16 x 16 grid of divs inside the gridContainer
+    // create a 16 x 16 grid of square divs inside the gridContainer
 
-    for (let i=1; i<=dimension*dimension; i++) {
-        gridContainer.appendChild(document.createElement("div"))
+    for (let i=0; i<dimension; i++) {
+        const row = document.createElement("div")
+        row.className = "grid-row"
+        gridContainer.appendChild(row)
+        for (let j=0; j<dimension; j++) {
+            const square = document.createElement("div")
+            square.className = "grid-square"
+            row.appendChild(square)
+        }
+        
     }
 }
 
