f71054c4ef1df835742c6893641f61b0de48dc41
Kexin Huang <khuan24@uwo.ca>
2025-01-07 13:21:48 -0500

    Implment a basic make grid function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const gridContainer = document.querySelector(".grid-container")
-
+const dimension = 16
 
 function makeGrid() {
     // create a 16 x 16 grid of divs inside the gridContainer
-    
+
+    for (let i=1; i<=dimension*dimension; i++) {
+        gridContainer.appendChild(document.createElement("div"))
+    }
 }
 
 makeGrid()
